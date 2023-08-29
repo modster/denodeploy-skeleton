ab75ccd5e5f7cae7a77a26025d7b62efe09d715d
E. M. Greeff <emgreeff@datadash.cloud>
2023-08-29 05:57:35 -0600

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.postcss';
+
+	// Highlight JS
+	import hljs from 'highlight.js';
+	import 'highlight.js/styles/github-dark.css';
+	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+	storeHighlightJs.set(hljs);
+</script>
+
+<slot />
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+import { join } from 'path'
+
+import typography from '@tailwindcss/typography';
+import { skeleton } from '@skeletonlabs/tw-plugin'
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		typography,
+		skeleton({
+			themes: {
+				preset: [
+					{
+						name: 'hamlindigo',
+						enhancements: true,
+					},
+					{
+						name: 'modern',
+						enhancements: true,
+					},
+					{
+						name: 'skeleton',
+						enhancements: true,
+					},
+					{
+						name: 'wintry',
+						enhancements: true,
+					},
+				],
+			},
+		}),
+	],
+};
